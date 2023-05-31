b5d5060f3714ef2cddaa318dadd511c886260dd3
aigerimzharylkassyn <133566749+aigerimzharylkassyn@users.noreply.github.com>
2023-06-01 00:31:30 +0600

    Add files via upload

---
+++ appProject/appProject/appProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  appProjectApp.swift
+//  appProject
+//
+//  Created by apple on 16.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct appProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
